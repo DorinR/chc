09aa2a456e3ae13c016ce80ced71b39716aa5c58
DorinsMac <dorin.rogov@groupecyberjustice.com>
2023-11-04 17:55:07 -0400

    contacts update adding tatiana rogova

--- src/Intl/6_contacts.tsx
+++ src/Intl/6_contacts.tsx
@@ -4,37 +4,52 @@ export const page6_contactsMessages: Translation[] = [
   [
     `contacts1`,
     `<h1>Republic of Moldova</h1>
-    <p>Name: Tudor Cujba</p>
+    <h2>Tudor Cujba</h2>
     <p>Phone: +37368852588</p>
     <p>Email: <a href="mailto:tudorcujba1974@gmail.com">tudorcujba1974@gmail.com</a></p>
     <p>Address: Negruzzi 4/2 boulevard, Chisinau, Rep of Moldova, MD 2001</p>
   
+    <h2>Tatiana Rogova</h2>
+    <p>Phone: +37368147678</p>
+    <p>Email: <a href="mailto:tatiana.rogova@dlr.md">tatiana.rogova@dlr.md</a></p>
+    <p>Address: bvd. C. Negruzzi 4/2, Chisinau, Rep of Moldova, MD-2001</p>
+  
     <h1>Canada</h1>
-    <p>Name: Alexei Rogov</p>
+    <h2>Alexei Rogov</h2>
     <p>Phone: +1 4385017075</p>
     <p>Email: <a href="mailto:alexeirogov@yahoo.ca">alexeirogov@yahoo.ca</a></p>
     <p>Address: 4175 RUE LEGAULT APT 405, SAINT-HUBERT PQ J3Y 0W5</p>`,
 
     `<h1>République de Moldavie</h1>
-    <p>Nom : Tudor Cujba</p>
+    <h2>Tudor Cujba</h2>
     <p>Téléphone : +37368852588</p>
     <p>Email : <a href="mailto:tudorcujba1974@gmail.com">tudorcujba1974@gmail.com</a></p>
     <p>Adresse : Negruzzi 4/2 boulevard, Chisinau, Rép de Moldavie, MD 2001</p>
+  
+    <h2>Tatiana Rogova</h2>
+    <p>Téléphone : +37368147678</p>
+    <p>Email : <a href="mailto:tatiana.rogova@dlr.md">tatiana.rogova@dlr.md</a></p>
+    <p>Adresse : bvd. C. Negruzzi 4/2, Chisinau, Rép de Moldavie, MD-2001</p>
     
     <h1>Canada</h1>
-    <p>Nom : Alexei Rogov</p>
+    <h2>Alexei Rogov</h2>
     <p>Téléphone : +1 4385017075</p>
     <p>Email : <a href="mailto:alexeirogov@yahoo.ca">alexeirogov@yahoo.ca</a></p>
     <p>Adresse : 4175 RUE LEGAULT APT 405, SAINT-HUBERT PQ J3Y 0W5</p>`,
 
     `<h1>Republica Moldova</h1>
-    <p>Nume: Tudor Cujba</p>
+    <h2>Tudor Cujba</h2>
     <p>Telefon: +37368852588</p>
     <p>Email: <a href="mailto:tudorcujba1974@gmail.com">tudorcujba1974@gmail.com</a></p>
     <p>Adresa: Negruzzi 4/2 bulevard, Chisinau, Rep Moldova, MD 2001</p>
+  
+    <h2>Tatiana Rogova</h2>
+    <p>Telefon: +37368147678</p>
+    <p>Email: <a href="mailto:tatiana.rogova@dlr.md">tatiana.rogova@dlr.md</a></p>
+    <p>Adresa: bvd. C. Negruzzi 4/2, Chisinau, Rep Moldova, MD-2001</p>
     
     <h1>Canada</h1>
-    <p>Nume: Alexei Rogov</p>
+    <h2>Alexei Rogov</h2>
     <p>Telefon: +1 4385017075</p>
     <p>Email: <a href="mailto:alexeirogov@yahoo.ca">alexeirogov@yahoo.ca</a></p>
     <p>Adresa: 4175 RUE LEGAULT APT 405, SAINT-HUBERT PQ J3Y 0W5</p>`,
